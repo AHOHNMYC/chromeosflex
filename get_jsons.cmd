119fdb1cd216614f9e7079b0000deea2c823fbd4
AHOHNMYC <24810600+AHOHNMYC@users.noreply.github.com>
2022-02-19 04:47:52 +0300

    meow

---
+++ get_jsons.cmd
@@ -0,0 +1,4 @@
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/onhub_recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/workspaceHardware_recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/cloudready_recovery.json
