a3c073791c93441315ef026c9c9ef2fe8c8b36b5
AHOHNMYC <24810600+AHOHNMYC@users.noreply.github.com>
2022-11-09 21:52:05 +0300

    json update

--- get_jsons.cmd
+++ get_jsons.cmd
@@ -1,4 +1,4 @@
-wget https://dl.google.com/dl/edgedl/chromeos/recovery/onhub_recovery.json
-wget https://dl.google.com/dl/edgedl/chromeos/recovery/workspaceHardware_recovery.json
-wget https://dl.google.com/dl/edgedl/chromeos/recovery/recovery.json
-wget https://dl.google.com/dl/edgedl/chromeos/recovery/cloudready_recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/onhub_recovery.json -O onhub_recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/workspaceHardware_recovery.json -O workspaceHardware_recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/recovery.json -O recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/cloudready_recovery.json -O cloudready_recovery.json
