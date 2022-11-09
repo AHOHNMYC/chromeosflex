cf5b7c3b9e76c1b09329dcb4679c6a1ebda3c925
AHOHNMYC <24810600+AHOHNMYC@users.noreply.github.com>
2022-11-09 23:20:13 +0300

    Added jsons from updated extension

--- get_jsons.cmd
+++ get_jsons.cmd
@@ -1,4 +1,7 @@
 wget https://dl.google.com/dl/edgedl/chromeos/recovery/onhub_recovery.json -O onhub_recovery.json
 wget https://dl.google.com/dl/edgedl/chromeos/recovery/workspaceHardware_recovery.json -O workspaceHardware_recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/workspaceHardware_recovery2.json -O workspaceHardware_recovery2.json
 wget https://dl.google.com/dl/edgedl/chromeos/recovery/recovery.json -O recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/recovery2.json -O recovery2.json
 wget https://dl.google.com/dl/edgedl/chromeos/recovery/cloudready_recovery.json -O cloudready_recovery.json
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/cloudready_recovery2.json -O cloudready_recovery2.json
